563c3b8d8e3a652e818c7b3a7cac1343008dc584
Nikita Dudnik <nikdudnik@gmail.com>
2023-05-27 00:11:57 +0200

    Refactor and add displacement frag shader

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
+import { Component, onMount } from 'solid-js';
 import styles from './App.module.css';
 
-const App: Component = () => {
+import initRegl from 'regl'
+//@ts-ignore
+import initMouse from 'mouse-change'
+
+import frag from "./shaders/feedback.frag?raw"
+import vert from "./shaders/basic-uv.vert?raw"
+
+function makeDrawFeedback(regl: initRegl.Regl, mouse: any, pixels: initRegl.Texture2D)
+  {return regl({
+    frag,
+
+    vert,
+
+    attributes: {
+      position: [
+        -2, 0,
+        0, -2,
+        2, 2]
+    },
+
+    uniforms: {
+      texture: pixels,
+      mouse: ({ pixelRatio, viewportHeight }) => [
+        mouse.x * pixelRatio,
+        viewportHeight - mouse.y * pixelRatio
+      ],
+      t: ({ tick }) => 0.01 * tick
+    },
+
+    count: 3
+  })}
+
+
+function App() {
+  let divRef: HTMLDivElement | undefined
+  onMount(() => {
+    if (!divRef) return;
+    const regl = initRegl()
+    const mouse = initMouse()
+
+    const pixels = regl.texture()
+
+    const drawFeedback = makeDrawFeedback(regl, mouse, pixels) 
+
+    regl.frame(function () {
+      regl.clear({
+        color: [0, 0, 0, 1]
+      })
+
+      drawFeedback()
+
+      pixels({
+        copy: true
+      })
+    })
+  })
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div ref={divRef!} class={styles.App}>
     </div>
   );
-};
+}
 
 export default App;
